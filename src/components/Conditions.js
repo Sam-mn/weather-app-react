b124d6250f8079faeb521f3822c9f31ce2ab1298
samer munawwar <samer.mnr@gmail.com>
2020-03-02 12:49:53 +0100

    adding h6 to conditions

--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -3,11 +3,14 @@ class Conditions extends React.Component {
 	state = {};
 	render() {
 		return (
-			<ul className="weather-list">
-				{this.props.weather.map((w, i) => (
-					<li key={i}>{w.description}</li>
-				))}
-			</ul>
+			<div>
+				<h6>Current weather is:</h6>
+				<ul className="weather-list">
+					{this.props.weather.map((w, i) => (
+						<li key={i}>{w.description}</li>
+					))}
+				</ul>
+			</div>
 		);
 	}
 }
